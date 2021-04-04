1960f7938a12badf71ceee5362bf064c3655bc29
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-04-04 19:48:43 +0530

    form page css changes

--- src/Pages/Form/Form.js
+++ src/Pages/Form/Form.js
@@ -5,6 +5,7 @@ import './Form.css';
 import Header from './Header';
 import Grid from '@material-ui/core/Grid';
 import salty from './Saly-14.svg';
+import uploadbutton from './upload.png';
 
 
 export default function Form() {
@@ -15,31 +16,38 @@ export default function Form() {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Header/>
-<Grid container spacing={5}>
+<Grid container spacing={2}>
 
 <Grid item xs={8} sm={4}><input {...register("Name", {required:true , maxLength:30})} placeholder="name" className="TopLabels"/></Grid>
 <Grid item xs={8} sm={4}><input {...register("address1", {required:true , maxLength:30})} placeholder="address line 1" className="TopLabels" /></Grid>
 </Grid>
-<Grid container spacing={5}>
+<Grid container spacing={2}>
 <Grid item xs={8} sm={4} ><input {...register("address2", {required:true , maxLength:30})} placeholder="address line 2" className="TopLabels" /></Grid>
 <Grid item xs={8} sm={4}><input {...register("city",{required:true , maxLength:20})} placeholder="city" className="TopLabels" /></Grid>
 </Grid>
-<Grid container spacing={5}>
+<Grid container spacing={2}>
 <Grid item xs={8} sm={4}><input {...register("email")} placeholder="Email ID" className="TopLabels"/></Grid>
 <Grid item xs={8} sm={4}><input {...register("City")} placeholder="City" className="TopLabels" /></Grid>
 </Grid>
-<Grid container spacing={5}>
-<Grid item xs={8} sm={2} ><input type="file" {...register("Resume")} placeholder="Resume" className="BottomLabels" /></Grid>
-<Grid item xs={8} sm={2} ><input type="file" {...register("Picture")} placeholder="Picture" className="BottomLabels" /></Grid>
+<Grid container spacing={2}>
+<Grid item xs={8} sm={2} >
+  <div className="image-upload BottomLabels input-style"> <label for="resume-file-input">
+  <span><img src= {uploadbutton} />Resume</span>
+    </label><input type="file" {...register("Resume")} placeholder="Resume" className="BottomLabels" id="resume-file-input"/></div></Grid>
+    
+<Grid item xs={8} sm={2} >
+<div className="image-upload BottomLabels input-style"> <label for="picture-file-input">
+        <span><img src= {uploadbutton}/>Picture</span>
+    </label><input type="file" {...register("Picture")} placeholder="Picture" className="BottomLabels" id="picture-file-input"/></div></Grid>
 <Grid item xs={8} sm={2} ><input {...register("bio")} placeholder="State" className="BottomLabels"/></Grid>
 <Grid item xs={8} sm={2} ><input {...register("state")} placeholder="Pincode" className="BottomLabels"/></Grid>
 </Grid>
-<Grid container spacing={5}>
+<Grid container spacing={2}>
 <Grid item xs={8} sm={2}><input {...register("size")} placeholder="Tshirt Size" className="LowerLabels"/></Grid>
 <Grid item xs={8} sm={2} ><input {...register("bio")} placeholder="Bio" className="LowerLabels"/></Grid>
 <Grid item xs={8} sm={4} ><input {...register("email")} placeholder="Email" className="LowerLabels"/></Grid>
 </Grid>
-<Grid container spacing={5}>
+<Grid container spacing={2}>
 <Grid item xs={8} sm={3}><input {...register("bio")} placeholder="Bio" className="Lower"/></Grid>
 
 <Grid item xs={12} sm={8}><input type="submit" /></Grid>
@@ -51,12 +59,14 @@ export default function Form() {
           height: "100vh",
           position: 'fixed',
           bottom: '-5vh',
-          right: '15vh',
+          right: '10vh',
+          zIndex: -1
         }}>
           <img style={{
             maxWidth: '100%',
             maxHeight: '100%',
             height: "auto",
+            zIndex: -1
            
           }} src={salty}  alt={"image"} className="From-img"/>
         </div>
