95b4e4ae2b10c64d84aa24ceb90c81ec61e2922a
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-04-05 21:39:53 +0530

    made the user profile page with some minor changes left

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,9 +1,65 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-
-export default function Profile() {
-
+import  user_photo from './Group 7.png';
+import {Grid} from '@material-ui/core'
+import './Profile.css';
+import side_photo from './Saly-1 (2).svg'
+export default function Profile() 
+{
   return (
-    <div>Profile Page</div>
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={8} sm={1} >
+      <img src={user_photo} alt="user photo" className="user_profile_picture"/>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={8} sm={12} >
+      <h1>Siddharth Singh</h1>
+      </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={8} sm={12}> 
+     				<h3>abc@gmail.com</h3>
+				</Grid>	
+			</Grid>
+
+      <Grid container spacing={2}>
+        <Grid item xs={8} sm={12} >
+      		<h3>+91 99999999</h3>
+			</Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={8} sm={12}>
+                <div className="team-btn-div">
+                      <button className="team-primary-btn">
+                        Edit Profile
+                      </button>
+							</div>
+				</Grid>
+      </Grid>
+      <div
+        className={"bkg"}
+        style={{
+         width: "80vw",
+          position: "fixed",
+          bottom: "-5vh",
+          right: "-1vh",
+          zIndex: -1,
+        }}
+      >
+        <img
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+            height: "auto",
+            zIndex: -1,
+          }}
+          src={side_photo}
+          alt={"image"}
+          className="From-img"
+        />
+      </div>
+
+    </div>
   );
 }
