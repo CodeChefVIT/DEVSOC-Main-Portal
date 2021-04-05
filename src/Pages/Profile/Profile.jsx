328962406c2d713dd56061e73865c4169ba90da8
Vinayak Gupta <guptavinayakg@gmail.com>
2021-04-06 01:06:25 +0530

    Profile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,65 +1,47 @@
 import React from "react";
-import  user_photo from './Group 7.png';
-import {Grid} from '@material-ui/core'
-import './Profile.css';
-import side_photo from './Saly-1 (2).svg'
-export default function Profile() 
-{
+import { Avatar, Grid, Hidden } from "@material-ui/core";
+import "./Profile.css";
+import back from "./back.svg";
+import { Link } from "react-router-dom";
+
+export default function Profile({ data }) {
   return (
-    <div>
-      <Grid container spacing={2}>
-        <Grid item xs={8} sm={1} >
-      <img src={user_photo} alt="user photo" className="user_profile_picture"/>
+    <div className="team-joined-div">
+      <Grid container spacing={3}>
+        <Grid item md={6}>
+          <Avatar
+            className="user_profile_picture"
+            alt={data.name}
+            variant="circular"
+            src={data.avatar}
+          />
+
+          <h1>{data.name}</h1>
+          <h3>{data.email}</h3>
+          <Link to="/app/profile/edit">
+            <button className="team-primary-btn">Edit Profile</button>
+          </Link>
         </Grid>
       </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={8} sm={12} >
-      <h1>Siddharth Singh</h1>
-      </Grid>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={8} sm={12}> 
-     				<h3>abc@gmail.com</h3>
-				</Grid>	
-			</Grid>
 
-      <Grid container spacing={2}>
-        <Grid item xs={8} sm={12} >
-      		<h3>+91 99999999</h3>
-			</Grid>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={8} sm={12}>
-                <div className="team-btn-div">
-                      <button className="team-primary-btn">
-                        Edit Profile
-                      </button>
-							</div>
-				</Grid>
-      </Grid>
-      <div
-        className={"bkg"}
-        style={{
-         width: "80vw",
-          position: "fixed",
-          bottom: "-5vh",
-          right: "-1vh",
-          zIndex: -1,
-        }}
-      >
-        <img
-          style={{
-            maxWidth: "100%",
-            maxHeight: "100%",
-            height: "auto",
-            zIndex: -1,
-          }}
-          src={side_photo}
-          alt={"image"}
-          className="From-img"
-        />
-      </div>
-
+      <Hidden xsDown>
+        <Grid item md={6}>
+          <img
+            style={{
+              maxWidth: "100%",
+              maxHeight: "100%",
+              height: "auto",
+              position: "fixed",
+              bottom: "0",
+              right: "0",
+              zIndex: -1,
+            }}
+            src={back}
+            alt={"image"}
+            className="From-img"
+          />
+        </Grid>
+      </Hidden>
     </div>
   );
 }
