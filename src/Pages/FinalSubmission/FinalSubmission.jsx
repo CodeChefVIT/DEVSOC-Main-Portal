ea846b837beec1f725156f6c22a6328a3dc5f724
Vinayak Gupta <guptavinayakg@gmail.com>
2021-05-01 20:31:19 +0530

    file size 100mb

--- src/Pages/FinalSubmission/FinalSubmission.jsx
+++ src/Pages/FinalSubmission/FinalSubmission.jsx
@@ -73,6 +73,12 @@ function FinalSubmission({ data, refresh }) {
 
     if (file) {
       const zip = new FormData();
+      if (file.size > 100000000) {
+        setErrorText("ZIP file size cannot exceed 100mb!");
+        setErrorSnack(true);
+        setLoading(false);
+        return;
+      }
       zip.append("zip", file);
 
       try {
